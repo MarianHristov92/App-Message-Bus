e3f934b82416ed9f60b37ca05b37f3b6b7e1cbb8
Marian Hristov <marian.hristov@keoz.com>
2021-07-26 10:02:23 +0200

    test

---
+++ Demo/DataReceivedEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using RealDigital.AppMessageBus.Interfaces;
+
+namespace RealDigital.AppMessageBus.Models
+{
+    public class DataReceivedEvent : IMessage
+    {
+        public string Description { get; set; }
+        public bool IsOnMainThread { get; set; }
+
+        public DataReceivedEvent(string desc, bool main)
+        {
+            Description = desc;
+            IsOnMainThread = main;
+        }
+    }
+}
